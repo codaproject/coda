d0d1cbbbd32734811579b7c6c82d382ed8f6b399
Ben Gyori <ben.gyori@gmail.com>
2025-11-24 17:19:14 -0500

    Implement initial grounding module

---
+++ src/coda/grounding/__init__.py
@@ -0,0 +1,9 @@
+from gilda import ScoredMatch, Annotation
+
+
+class BaseGrounder:
+    def ground(self, text: str) -> list[ScoredMatch]:
+        raise NotImplementedError("This method should be overridden by subclasses.")
+
+    def annotate(self, text: str) -> list[Annotation]:
+        raise NotImplementedError("This method should be overridden by subclasses.")
