4d860dcf5dd97daf23ff3192d7fbefb2f40c1d3a
Ben Gyori <ben.gyori@gmail.com>
2025-12-02 10:00:41 -0800

    Improve HP processing

--- src/coda/kg/sources/hpo.py
+++ src/coda/kg/sources/hpo.py
@@ -1,14 +1,22 @@
 import pandas as pd
 import networkx as nx
+import obonet
 
+from coda import CODA_BASE
+
+HPO_BASE = CODA_BASE.module('hpo')
 HPOA_URL = "https://purl.obolibrary.org/obo/hp/phenotype.hpoa"
+HPO_URL = "https://purl.obolibrary.org/obo/hp.obo"
 
 # database_id»disease_name»···qualifier»··hpo_id»·reference»··evidence»···onset»··frequency»··sex»modifier»···aspect»·biocuration
 #      6 OMIM:619340»Developmental and epileptic encephalopathy 96»··»···HP:0011097»·PMID:31675180»··PCS»»···1/2»»···»···P»··HPO:probinson[2021-06-21]
 #      7 OMIM:619340»Developmental and epileptic encephalopathy 96»··»···HP:0002187»·PMID:31675180»··PCS»»···1/1»»···»···P»··HPO:probinson[2021-06-21]
 
 def get_hpoa_graph():
-    df = pd.read_csv(HPOA_URL, sep='\t', skiprows=4)
+    hpoa_file = HPO_BASE.ensure(url=HPOA_URL)
+    hpo_file = HPO_BASE.ensure(url=HPO_URL)
+    df = pd.read_csv(hpoa_file, sep='\t', skiprows=4)
+    hp_graph = obonet.read_obo(hpo_file)
     nodes = []
     edges = []
     disease_added = set()
@@ -33,9 +41,11 @@ def get_hpoa_graph():
             disease_added.add(disease_curie)
 
         if phenotype_curie not in phenotypes_added:
+            hp_term_name = hp_graph.nodes[hpo_id]['name']
             nodes.append([
                 phenotype_curie, {
-                    'kind': 'hp'
+                    'kind': 'hp',
+                    'name': hp_term_name,
                 }
             ])
             phenotypes_added.add(phenotype_curie)
