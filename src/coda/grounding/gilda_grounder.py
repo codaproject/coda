bd10cfed1ae965b83ad53eabba1453eca59a31ef
Ben Gyori <ben.gyori@gmail.com>
2026-03-19 09:26:30 -0400

    Refine namespaces for Gilda grounding

--- src/coda/grounding/gilda_grounder.py
+++ src/coda/grounding/gilda_grounder.py
@@ -2,10 +2,19 @@ import gilda
 
 from . import BaseGrounder
 
+DEFAULT_NAMESPACES = ['MESH', 'DOID', 'HP']
+
 
 class GildaGrounder(BaseGrounder):
+    """Wrapper for using Gilda as the grounding system."""
+
+    def __init__(self, namespaces: list = None):
+        super().__init__()
+        self.namespaces = DEFAULT_NAMESPACES \
+            if namespaces is None else namespaces
+
     def ground(self, text: str) -> list:
-        return gilda.ground(text)
+        return gilda.ground(text, namespaces=self.namespaces)
 
     def annotate(self, text: str) -> list:
-        return gilda.annotate(text)
+        return gilda.annotate(text, namespaces=self.namespaces)
